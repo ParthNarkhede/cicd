f85d2c4c9693dbf0c48cc51de15b3dd567f9a360
ParthNarkhede <parthnarkhede1120@gmail.com>
2025-04-30 00:25:42 +0530

    The changes related to AWS EKS deployment and a change in app.py is made

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask app deployed using GitHub Actions and Kubernetes!"
+    return "Hello from Flask app deployed using GitHub Actions and Kubernetes! Very Welcome from first deployment"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
